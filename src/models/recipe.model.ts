17966c7297cfc64a300784cbc2fe136e274bfe97
PepCarmona <pep.carmona.coll@gmail.com>
2021-09-08 00:28:50 +0200

    update recipeSchema

--- src/models/recipe.model.ts
+++ src/models/recipe.model.ts
@@ -1,14 +1,70 @@
 import { Document, model, Schema } from 'mongoose';
 
+interface RecipeTime {
+  preparation?: number;
+  cooking: number;
+}
+
+interface Ingredient {
+  quantity: number;
+  units: string;
+  name: string;
+}
+
+interface Step {
+  position: number;
+  content: string;
+}
 export interface IRecipe {
   title: string;
-  body: string;
+  description?: string;
+  time: RecipeTime;
+  servings: number;
+  ingredients: Ingredient[];
+  instructions: Step[];
+  tags: string[];
 }
+
+const RecipeTimeSchema = new Schema<RecipeTime>(
+    {
+        preparation: Number,
+        cooking: {
+            type: Number,
+            required: true,
+        }
+    }
+);
+
+const IngredientSchema = new Schema<Ingredient>(
+    {
+        quantity: Number,
+        units: String,
+        name: String,
+    }
+);
+
+const StepSchema = new Schema<Step>(
+    {
+        position: Number,
+        content: String,
+    }
+);
 
 const RecipeSchema = new Schema<IRecipe>(
     {
-        title: String,
-        body: String,
+        title: {
+            type: String,
+            required: true,
+        },
+        description: String,
+        time: RecipeTimeSchema,
+        servings: {
+            type: Number,
+            default: 4,
+        },
+        ingredients: [IngredientSchema],
+        instructions: [StepSchema],
+        tags: [String],
     }
 );
 
