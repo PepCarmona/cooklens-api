0646cf3fc7d103ed43636990bd4a79323ca4aead
PepCarmona <pep.carmona.coll@gmail.com>
2021-09-16 08:53:43 +0200

    modify if-else structure

--- src/routes/recipe.route.ts
+++ src/routes/recipe.route.ts
@@ -60,60 +60,61 @@ recipeRouter.route('/add').post((req, res) => {
 
     if (!req.body || Object.keys(req.body).length === 0) {
         res.status(404).send('Cannot save empty objects');
+        return;
     }
-    else {
-        recipe
-            .save()
-            .then((recipe: IRecipe) => {
-                res.status(200).json(recipe);
-            })
-            .catch(() => res.status(400).send('Unable to save item to database'));
-    }
+    
+    recipe
+        .save()
+        .then((recipe: IRecipe) => {
+            res.status(200).json(recipe);
+        })
+        .catch(() => res.status(400).send('Unable to save item to database'));
 });
 
 recipeRouter.route('/update').put((req, res) => {
     if (!req.body || Object.keys(req.body).length === 0) {
         res.status(404).send('Cannot save empty objects');
+        return;
     }
-    else if (!req.query.id) {
+    if (!req.query.id) {
         res.status(404).send('Document Id not provided');
+        return;
     }
-    else {
-        Recipe
-            .findByIdAndUpdate(req.query.id, req.body, {new: true})
-            .then((recipe: IRecipe | null) => {
-                if (!recipe) {
-                    res.status(404).send('Document with provided id not found');
-                }
-                else {
-                    res.status(200).json(recipe);
-                }
-            })
-            .catch((err) => {
-                res.status(404).json(err);
-            });
-    }
+    
+    Recipe
+        .findByIdAndUpdate(req.query.id, req.body, {new: true})
+        .then((recipe: IRecipe | null) => {
+            if (!recipe) {
+                res.status(404).send('Document with provided id not found');
+            }
+            else {
+                res.status(200).json(recipe);
+            }
+        })
+        .catch((err) => {
+            res.status(404).json(err);
+        });
 });
 
 recipeRouter.route('/delete').delete((req, res) => {
     if (!req.query.id) {
         res.status(404).send('Document Id not provided');
+        return;
     }
-    else {
-        Recipe
-            .findByIdAndDelete(req.query.id)
-            .then((recipe: IRecipe | null) => {
-                if (!recipe) {
-                    res.status(404).send('Document with provided id not found');
-                }
-                else {
-                    res.status(200).json(recipe);
-                }
-            })
-            .catch((err) => {
-                res.status(404).json(err);
-            });
-    }
+    
+    Recipe
+        .findByIdAndDelete(req.query.id)
+        .then((recipe: IRecipe | null) => {
+            if (!recipe) {
+                res.status(404).send('Document with provided id not found');
+            }
+            else {
+                res.status(200).json(recipe);
+            }
+        })
+        .catch((err) => {
+            res.status(404).json(err);
+        });
 });
 
 recipeRouter.route('/import').get((req, res) => {
@@ -180,7 +181,7 @@ recipeRouter.route('/integrated-sites').get((req, res) => {
         res.status(404);
         return;
     }
-    
+
     res.status(200).json(integratedSites);
 });
 
