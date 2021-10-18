f81f70b8197fc68d7c1698cf1a3942e70139c53b
PepCarmona <pep.carmona.coll@gmail.com>
2021-10-18 12:37:44 +0200

    Create update week plan endpoint

--- src/routes/mealPlan.route.ts
+++ src/routes/mealPlan.route.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { CustomError } from '../helpers/errors';
 import { paginate } from '../helpers/pagination';
 import authMiddleware, { RequestWithUserDecodedToken } from '../middleware/auth.middleware';
-import WeeklyPlan from '../models/mealPlan.models';
+import WeeklyPlan, { IWeeklyPlan } from '../models/mealPlan.models';
 import User from '../models/user.model';
 
 const mealPlanRouter = express.Router();
@@ -119,6 +119,30 @@ mealPlanRouter.route('/unsubscribeToWeekPlan').put(authMiddleware, (req: Request
         ));
 });
 
-// mealPlanRouter.route('/updateWeekPlan').put()
+mealPlanRouter.route('/updateWeekPlan').put(authMiddleware, (req: RequestWithUserDecodedToken, res) => {
+    const weekPlanId = req.query.id;
+    const weekPlan: IWeeklyPlan = req.body;
+    const user = req.decoded!.user;
+
+    if (!weekPlan || Object.keys(weekPlan).length === 0) {
+        return res.status(400).json(new CustomError('Cannot save empty objects'));
+    }
+
+    WeeklyPlan
+        .findOneAndUpdate(
+            { _id: weekPlanId, author: user }, weekPlan, { new: true })
+        .then((updatedWeekPlan) => {
+            if (!updatedWeekPlan) {
+                return res.status(404).json(
+                    new CustomError('Document with provided id not found or not enough permissions to update it')
+                );
+            }
+
+            res.status(200).json(updatedWeekPlan);
+        })
+        .catch((err) => {
+            res.status(500).json(new CustomError('Could not find week plan by id or update it', err));
+        });
+});
 
 export default mealPlanRouter;
