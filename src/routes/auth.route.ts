e4a23eb651f8f02274870a2f58d459e5e66a8833
PepCarmona <pep.carmona.coll@gmail.com>
2021-12-21 11:00:31 +0100

    send confirmation mail on register

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -4,6 +4,7 @@ import User, { SigninForm, SignupForm } from '../models/user.model';
 import { sign, verify } from 'jsonwebtoken';
 import { CustomError } from '../helpers/errors';
 import { sendMail } from '../helpers/mail';
+import { URL } from 'url';
 
 const authRouter = express.Router();
 authRouter.route('/mail').post((req, res) => {
@@ -20,7 +21,7 @@ authRouter.route('/mail').post((req, res) => {
 				background: linear-gradient(0deg,transparent 0%,#00000070 100%);
 				color: white;
 				padding: 20px;
-				padding-bottom: 200px;
+				padding-bottom: 100px;
 				text-align: center;
 			">
 				<span style="
@@ -79,10 +80,6 @@ authRouter.route('/mail').post((req, res) => {
 		.catch((err) =>
 			res.status(400).json({
 				customerror: new CustomError('Error sending email', err),
-				auth: {
-					user: process.env.GMAIL_USER,
-					pass: process.env.GMAIL_PASSWORD,
-				},
 			})
 		);
 });
@@ -133,18 +130,100 @@ authRouter.route('/signup').post((req, res) => {
 					user
 						.save()
 						.then((savedUser) => {
-							sign(
-								{ savedUser },
-								process.env.JWTSECRET!,
-								{ expiresIn: 31556926 },
-								(err, token) => {
-									res.status(200).json({
-										// @ts-ignore
-										user: { ...savedUser._doc, password: undefined },
-										token,
-									});
-								}
+							const confirmationUrl = new URL(
+								'https://cooklens.pepcarmona.com/confirmRegistry'
 							);
+							confirmationUrl.searchParams.append('code', confirmationCode);
+
+							sendMail({
+								to: 'pep.carmona.coll@gmail.com',
+								html: `
+								<div style="
+									background-image: url('https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80');
+									background-position: center;
+									background-size: cover;
+									padding-bottom: 40px;
+								">
+									<div style="
+										background: linear-gradient(0deg,transparent 0%,#00000070 100%);
+										color: white;
+										padding: 20px;
+										padding-bottom: 100px;
+										text-align: center;
+									">
+										<span style="
+											font-size: 50px;
+										">
+											Cooklens
+										</span>
+										<br/>
+										<span style="
+											font-size: 20px;
+										">
+											Great things await us!!
+										</span>
+									</div>
+									<div style="
+										width: 50%;
+										min-width: 200px;
+										max-width: 500px;
+										background-color: #00000045;
+										border-radius: 10px;
+										margin-left: auto;
+										margin-right: auto;
+										padding: 20px;
+										font-size: 16px;
+										color: #F2F3F4;
+										box-shadow: 0 0 10px 1px #00000045;
+										backdrop-filter: blur(5px);
+									">
+										<div>
+											<p>
+												Hello ${savedUser.username}, <br/><br/>
+												We are glad to see you joining our community. <br/>
+												You are just a step away from being part of this amazing network!
+											</p>
+											<br/>
+											<a 
+												href="${confirmationUrl}"
+												style="
+													display: block;
+													background-color: #12B886;
+													color: white;
+													text-decoration: none;
+													padding: 8px 20px;
+													border-radius: 10px;
+													width: fit-content;
+												"
+											>
+												Confirm email address
+											</a>
+										</div>
+									</div>
+								</div>
+								`,
+							})
+								.then(() =>
+									sign(
+										{ savedUser },
+										process.env.JWTSECRET!,
+										{ expiresIn: 31556926 },
+										(err, token) => {
+											res.status(200).json({
+												// @ts-ignore
+												user: { ...savedUser._doc, password: undefined },
+												token,
+											});
+										}
+									)
+								)
+								.catch((err) =>
+									res
+										.status(400)
+										.json(
+											new CustomError('Error sending confirmation mail', err)
+										)
+								);
 						})
 						.catch((err) =>
 							res
